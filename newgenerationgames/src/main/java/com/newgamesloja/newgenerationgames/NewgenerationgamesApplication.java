af288fb8c06d03444018c9840bf154efe9371ed5
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-02-24 20:42:02 -0300

    SpringBoot Main

---
+++ newgenerationgames/src/main/java/com/newgamesloja/newgenerationgames/NewgenerationgamesApplication.java
@@ -0,0 +1,13 @@
+package com.newgamesloja.newgenerationgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgenerationgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewgenerationgamesApplication.class, args);
+	}
+
+}
---
+++ newgenerationgames/src/test/java/com/newgamesloja/newgenerationgames/NewgenerationgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newgamesloja.newgenerationgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewgenerationgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
